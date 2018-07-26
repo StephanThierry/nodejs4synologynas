3767ee9db72b7d7e1aa498f223527e02b4d332b5
Stephan Thierry <stephan.thierry@live.com>
2018-07-26 20:47:08 +0200

    Scripts and .js files from README

---
+++ scripts/nodeserverstart.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+PATH=$PATH:/volume1/@appstore/Node.js_v8/usr/local/lib/node_modules/forever/bin
+
+start() {
+        forever start /volume1/server/HelloWorldServer -l /volume1/server/HelloWorldServer/logs/log.txt  -o /volume1/server/HelloWorldServer/logs/output.txt
+}
+
+stop() {
+        killall -9 node
+}
+
+case "$1" in
+  start)
+    start
+    ;;
+  stop)
+    stop
+    ;;
+  *)
+    echo "Usage: $0 {start|stop}"
